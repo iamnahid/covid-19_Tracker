2d9fa46158acf7be805fee12a8d1bdb9c143b7bb
iamnahid <nrk.n9@yahoo.com>
2020-04-07 22:44:39 +0600

    Updates

---
+++ precache-manifest.75bafafac054176595e8e82ed4f57257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a3e2ef34f9ddc40f0f647c82b72d20",
+    "url": "/covid-19_Tracker/index.html"
+  },
+  {
+    "revision": "4accdbb1c45ed90a5594",
+    "url": "/covid-19_Tracker/static/css/main.9430e60b.chunk.css"
+  },
+  {
+    "revision": "ab9831ef9ad4ca18b43c",
+    "url": "/covid-19_Tracker/static/js/2.736caa78.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19_Tracker/static/js/2.736caa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4accdbb1c45ed90a5594",
+    "url": "/covid-19_Tracker/static/js/main.1a9c7049.chunk.js"
+  },
+  {
+    "revision": "6af117fe1fabfaca16ba",
+    "url": "/covid-19_Tracker/static/js/runtime-main.7aefb849.js"
+  }
+]);
